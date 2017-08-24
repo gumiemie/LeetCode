7a08c62e474f61507d8ce494feedb9698604d294
guyang <guyang@ebnew.com>
2017-08-24 14:36:34 +0800

    1 day

--- src/main/java/001-Two Sum/TwoSum.java
+++ src/main/java/001-Two Sum/TwoSum.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 /**
  * @author 顾洋 <guyang@ebnew.com>
  * @description Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -5,6 +7,31 @@
  * @date 2017/8/24 14:00$
  */
 public class TwoSum {
+
+    @Test
+    public void test(){
+        //[3,2,4] 6
+        int[] array = new int[]{3,2,4};
+        int target = 6;
+
+        int[] ints = twoSum(array, target);
+        System.out.print("["+ints[0]+","+ints[1]+"]");
+    }
+
+    //解决思路是两个for循环.
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
 
 }
 
---
+++ src/main/java/002-AddTwoNumbers/AddTwoNumbers.java
@@ -0,0 +1,7 @@
+/**
+ * @author 顾洋 <guyang@ebnew.com>
+ * @description 用一句话描述这个类
+ * @date 2017/8/24 14:23$
+ */
+public class AddTwoNumbers {
+}
