848e37aa9dc350f449a3040296ce2ed8935a5c99
guyang <guyang@ebnew.com>
2017-09-20 14:55:59 +0800

    problem 5 update

--- 005-Longest Palindromic Substring/LongestPalindromicSubstring.java
+++ 005-Longest Palindromic Substring/LongestPalindromicSubstring.java
@@ -8,62 +8,50 @@ import org.junit.Test;
 public class LongestPalindromicSubstring {
 
     public String longestPalindrome(String s) {
-        if (s == null || "".equals(s)) return "";
+        if (s == null || "".equals(s) || s.length() == 1) return s;
         String finalResult = "";
         String result = "";
-        //嵌套循环,外层控制截取起始下标,内层控制结束下标.
-        for (int i = 0; i < s.length(); i++) {
-            int temp = 0;
-            boolean flag = false;
-            for (int j = i + 1; j < s.length(); j++) {
-                result = s.substring(i, j);
-                String nextChar = s.substring(j, j + 1);
-                if (result.contains(nextChar)) {
-                    int index = result.indexOf(nextChar);
-                    if (temp == 0) {
-                        if (index == result.length() - 2) {
-                            flag = true;
-                            result = result + nextChar;
-                            temp = 2;
-                            continue;
-                        } else if (index == result.length() - 1) {
-                            result = result + nextChar;
-                            temp = 2;
-                            continue;
-                        } else {
-                            break;
+        //循环,控制回文字符串中心点.
+        one:
+        for (int i = 1; i < s.length() - 1; i++) {
+            int length = s.length();
+            //标识中心点是否为单中心或和前面的字符组成双中心,0=单中心,例如:"aba",1=双中心 例如:"abba"
+            if (s.substring(i - 1, i).equals(s.substring(i + 1, i + 2))) {
+                int x = i > length - 1 - i ? length - 1 - i : i;//最多截取的层数.
+                two:
+                for (int j = 1; j <= x; j++) {
+                    if (s.substring(i - j, i - j + 1).equals(s.substring(i + j, i + j + 1))) {
+                        result = s.substring(i - j, i + j + 1);
+                        if (result.length() > finalResult.length()) {
+                            finalResult = result;
                         }
                     } else {
-                        if (flag) {
-                            if (index == result.length() - 2 - temp) {
-                                temp += 2;
-                                result = result + nextChar;
-                                continue;
-                            }else {
-                                break;
-                            }
-                        } else {
-                            if (index == result.length() - 1 - temp) {
-                                result = result + nextChar;
-                                temp += 2;
-                                continue;
-                            }else {
-                                break;
-                            }
-                        }
+                        if (finalResult.length() > (2 * x + 1)) break one;
+                        break two;
                     }
-                } else {
-                    if (temp != 0) break;
                 }
             }
-            if (flag){
-                result = result.substring(result.length()-1 - temp);
-            }else {
-                result = result.substring(result.length() - temp);
-            }
-            if (finalResult.length()<result.length()) finalResult = result;
-            if (finalResult.length() > s.length() / 2) {
-                break;
+            if (s.substring(i - 1, i).equals(s.substring(i, i + 1))) {
+                int x = i - 1 > length - 1 - i ? length - 1 - i : i - 1;//最多截取的层数.
+                two:
+                for (int j = 1; j <= x; j++) {
+                    result = s.substring(i - 1, i + 1);
+                    if (result.length() > finalResult.length()) {
+                        finalResult = result;
+                    }
+                    int temp = 1;
+                    if (i > temp) {
+                        if (s.substring(i - j - temp, i - j - temp + 1).equals(s.substring(i + j, i + j + 1))) {
+                            result = s.substring(i - j - temp, i + j + 1);
+                            if (result.length() > finalResult.length()) {
+                                finalResult = result;
+                            }
+                        } else {
+                            if (finalResult.length() > (2 * x + 1 + temp)) break one;
+                            break two;
+                        }
+                    }
+                }
             }
         }
         return finalResult;
@@ -71,7 +59,7 @@ public class LongestPalindromicSubstring {
 
     @Test
     public void execute() {
-        String res = longestPalindrome("cbbd");
+        String res = longestPalindrome("abcba");
         System.out.println(res);
     }
 }
